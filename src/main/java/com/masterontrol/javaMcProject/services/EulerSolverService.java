e8ead4def392fbf36c0c81b20a6c65d4d0dcbc56
Gweaver <weavergarrett12@gmail.com>
2021-08-02 14:37:15 -0600

    Problem2 Commit

--- src/main/java/com/masterontrol/javaMcProject/services/EulerSolverService.java
+++ src/main/java/com/masterontrol/javaMcProject/services/EulerSolverService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 public class EulerSolverService {
     public EulerSolverService(){
         problem1();
+        problem2();
     }
     void problem1(){
         long total = 0;
@@ -16,7 +17,23 @@ public class EulerSolverService {
         }
         System.out.println(total);
     }
-    void problem2(){
+    void problem2() {
+        long fib1 = 1;
+        long fib2 = 2;
+        long total2 = fib2;
+        for (int i = 0; i < 4000000; i++) {
+            long sum = fib1 + fib2;
+            if (sum < 4000000) {
+                if (sum % 2 == 0) {
+                    total2 += sum;
+                }
+                fib1 = fib2;
+                fib2 = sum;
+            }
+        }
+        System.out.println(total2);
+    }
+    void problem3(){
 
     }
 }
